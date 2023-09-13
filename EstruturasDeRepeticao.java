daa0a42128e93c37f6e4486ea55f877acbf33e94
Tiago Mota Santos da Costa <tiagom0764@gmail.com>
2023-09-12 21:10:47 -0300

    ap05(repeticao): declaracao de variaveis

---
+++ EstruturasDeRepeticao.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+public class EstruturasDeRepeticao {
+    public static void main(String... abc) {
+       //Declaração de variáveis
+       //Inteiros: byte (1 Byte), short (2 Bytes), int (4 Bytes), long (8 Bytes)
+       short numeroAlunos, contador;
+       //float(4 bytes) e double (8 bytes)
+       float nota1, nota2;
+       //Entrada
+
+       //Processamento
+
+       //Saída
+    }
+}
